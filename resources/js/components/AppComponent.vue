a9d40343eb53f06002e6794c6da1e4158975653c
Monty Panday <montypanday293@gmail.com>
2019-06-26 00:36:34 +1000

    initial commit

---
+++ resources/js/components/AppComponent.vue
@@ -0,0 +1,10 @@
+<template>
+ <h2>AppComponent</h2>   
+</template>
+
+<script>
+export default {
+    
+}
+
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app">
+            <app-component></app-component>
+        </div>
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
